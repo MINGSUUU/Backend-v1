3558e260231d0e37c0269cac93281638b05b993e
hothuuk <s22014@gsm.hs.kr>
2023-12-16 21:57:08 +0900

    project create

---
+++ src/test/java/mingsuu/ColPoP/ColPoPApplicationTests.java
@@ -0,0 +1,13 @@
+package mingsuu.ColPoP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColPoPApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
