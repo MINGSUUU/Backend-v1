1e531483411e5b461ebc3ca062f75e0b940e56ec
hothuuk <s22014@gsm.hs.kr>
2023-12-18 23:51:17 +0900

    :memo: LoginRequest 작성

---
+++ src/main/java/mingsuu/ColPoP/domain/auth/presentation/dto/request/LoginRequest.java
@@ -0,0 +1,16 @@
+package mingsuu.ColPoP.domain.auth.presentation.dto.request;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class LoginRequest {
+
+    private String email;
+
+    private String password;
+}
