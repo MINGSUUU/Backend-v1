69b68f28a6acdb21ff9485a14c85ea01dec6c54c
hothuuk <s22014@gsm.hs.kr>
2023-12-19 00:39:38 +0900

    :memo: reissue 추가

--- src/main/java/mingsuu/ColPoP/domain/auth/presentation/AuthController.java
+++ src/main/java/mingsuu/ColPoP/domain/auth/presentation/AuthController.java
@@ -4,14 +4,13 @@ import lombok.RequiredArgsConstructor;
 import mingsuu.ColPoP.domain.auth.presentation.dto.request.LoginRequest;
 import mingsuu.ColPoP.domain.auth.presentation.dto.request.SignUpRequest;
 import mingsuu.ColPoP.domain.auth.presentation.dto.response.LoginResponse;
+import mingsuu.ColPoP.domain.auth.presentation.dto.response.NewTokenResponse;
 import mingsuu.ColPoP.domain.auth.service.LoginService;
+import mingsuu.ColPoP.domain.auth.service.ReIssueTokenService;
 import mingsuu.ColPoP.domain.auth.service.SignUpService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,6 +20,8 @@ public class AuthController {
     private final SignUpService signUpService;
 
     private final LoginService loginService;
+
+    private final ReIssueTokenService reIssueTokenService;
 
     @PostMapping("/signup")
     public ResponseEntity<Void> signup(@RequestBody SignUpRequest signUpRequest) {
@@ -32,5 +33,11 @@ public class AuthController {
     public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest loginRequest) {
         LoginResponse loginResponse = loginService.execute(loginRequest);
         return new ResponseEntity<>(loginResponse, HttpStatus.OK);
+    }
+
+    @PatchMapping("/reissue")
+    public ResponseEntity<NewTokenResponse> reIssueToken(@RequestHeader("Refresh-token") String refreshToken) {
+        NewTokenResponse newTokenResponse = reIssueTokenService.execute(refreshToken);
+        return new ResponseEntity<>(newTokenResponse, HttpStatus.OK);
     }
 }
