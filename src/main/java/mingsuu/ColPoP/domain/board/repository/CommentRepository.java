806db7ffdbcca2f13403fad5efe2ab3a5b851d7f
hothuuk <s22014@gsm.hs.kr>
2023-12-19 14:34:59 +0900

    :memo: findByBoard 추가

--- src/main/java/mingsuu/ColPoP/domain/board/repository/CommentRepository.java
+++ src/main/java/mingsuu/ColPoP/domain/board/repository/CommentRepository.java
@@ -1,8 +1,12 @@
 package mingsuu.ColPoP.domain.board.repository;
 
+import mingsuu.ColPoP.domain.board.entity.Board;
 import mingsuu.ColPoP.domain.board.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
+    List<Comment> findByBoard(Board board);
 }
