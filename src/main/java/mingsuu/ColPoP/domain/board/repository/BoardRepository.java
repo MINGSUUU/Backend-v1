c30fe40b585733d062ac48d09f7959b8517adbec
hothuuk <s22014@gsm.hs.kr>
2023-12-19 15:18:54 +0900

    :memo: findByAuthor 추가

--- src/main/java/mingsuu/ColPoP/domain/board/repository/BoardRepository.java
+++ src/main/java/mingsuu/ColPoP/domain/board/repository/BoardRepository.java
@@ -1,8 +1,12 @@
 package mingsuu.ColPoP.domain.board.repository;
 
 import mingsuu.ColPoP.domain.board.entity.Board;
+import mingsuu.ColPoP.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
 
+    List<Board> findByAuthor(User user);
 }
